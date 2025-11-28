be307ee5bdb0d1e44444823e9545d9e5127c1b1c
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-11-28 10:26:55 +0100

    fix(): Uniformisation des boutons

--- my-app/src/components/bouton/BoutonDanger.tsx
+++ my-app/src/components/bouton/BoutonDanger.tsx
@@ -18,13 +18,12 @@ export const BoutonDanger: React.FC<DangerButtonProps> = ({
       onClick={onClick}
       disabled={disabled}
       className={`
-        border border-red-100 dark:border-zinc-700 w-full inline-flex justify-center shadow-sm items-center px-2 py-1.5 text-sm font-medium text-red-700 dark:text-red-500 dark:hover:text-red-300 bg-red-50 dark:bg-zinc-800 rounded-md hover:bg-red-500 dark:hover:bg-red-800 hover:text-red-50 transition-colors cursor-pointer
-        ${
-          disabled
-            ? "opacity-50 cursor-not-allowed hover:bg-red-50 dark:hover:bg-red-900 hover:text-red-300"
-            : ""
-        }
-        ${className} 
+        w-full inline-flex justify-center shadow-sm items-center px-2 py-1.5 text-sm font-medium text-white 
+        bg-red-500 rounded-md transition
+        hover:bg-red-400 dark:hover:bg-red-800
+        disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-red-500 disabled:dark:hover:bg-red-500
+        ${!disabled ? "cursor-pointer" : ""}
+        ${className}
       `}
     >
       {children}
